38dd71e7e50ff0a737ba3cc6f600abe335df4d3e
ISsam777 <issam392004@gmail.com>
2024-12-17 15:52:14 +0100

    put files

--- install.sh
+++ install.sh
@@ -6,12 +6,10 @@ BBlack='\033[1;30m' BRed='\033[1;31m'    BGreen='\033[1;32m' BYellow='\033[1;33m
 BBlue='\033[1;34m'  BPurple='\033[1;35m' BCyan='\033[1;36m'  BWhite='\033[1;37m'
 
 #### install dependencies -----------------------
-
 echo -e ${BBlue}"\n [*] Installing dependencies......."
 
-sudo pacman -Sy yazi fish stow rofi polybar picom neovim fzf lazygit exa zoxide
-tmux kitty nerd-fonts betterlockscreen 
-sudo pacman -Sy dragon-drop
+pacman -S kitty yazi fish stow rofi polybar picom neovim fzf lazygit exa zoxide
+thefuck
 
 echo -e ${BPurple}"\n [*] synching files ......"
 
