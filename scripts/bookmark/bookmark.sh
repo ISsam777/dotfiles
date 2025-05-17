c5336267eb10021904dcb6b3ea863c359140341b
ISsam777 <issam392004@gmail.com>
2025-05-17 12:25:26 +0100

    edit

--- scripts/bookmark/bookmark.sh
+++ scripts/bookmark/bookmark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
-name=$(grep -Rw '/home/issam/dotfiles/scripts/bookmark/list.txt' -e ""|awk '{print $1}'|rofi -dmenu -p )
-grep -Rw '/home/issam/dotfiles/scripts/bookmark/list.txt' -e "$name"|awk '{print $2}'|rofi -dmenu -p 
-
+# name=$(grep -Rw '/home/issam/scripts/bookmark/list.txt' -e ""|awk '{print $1}'|rofi -dmenu -p )
+# zen-browser $(grep -Rw '/home/issam/scripts/bookmark/list.txt' -e $name|awk '{print $2}')
+grep -Rwi '/home/issam/scripts/bookmark/list.txt' -e ""|rofi -dmenu -p|awk '{print $2}'|xargs -r zen-browser
